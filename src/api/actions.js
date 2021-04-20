e0a6035d5ac08348cf39496ccfe63549d804386c
Alan Osta <alanosta@gmail.com>
2021-04-19 21:37:54 -0300

    Redux+Thunk funcionando

--- src/api/actions.js
+++ src/api/actions.js
@@ -16,50 +16,25 @@ function shuffle(array) {
 export const repartirMazo = () => {
     let cartasJugador = [];
     let cartasAdversario = [];
-
-    /*
-    let ajax = new XMLHttpRequest();
-    ajax.open("GET", "./lib/superAmigosDB.json"); 
-    ajax.addEventListener("load", repartirMazo);
-    ajax.send();
-    let mazo = JSON.parse(this.responseText);
-    */
-
+    let mazo;  
     let i;
     for (cartasJugador=[], i=0 ; i<34 ; ++i) cartasJugador[i]=i;
     cartasJugador = shuffle(cartasJugador);
     cartasAdversario = cartasJugador.splice(16, 17);
-    console.log(cartasJugador);
-    console.log(cartasAdversario);
     
-    return {
-        type: "REPARTIR_MAZO",
-        cartasJugador : cartasJugador,
-        cartasAdversario : cartasAdversario,
-        cartaJugador : {
-            "id": 1,
-            "lCarta": "A",
-            "nCarta": "1",
-            "nombre": "Superman",
-            "altura": 2.05,
-            "peso": 110,
-            "fuerza": 2000,
-            "ganadas": 990,
-            "velocidad": 400,
-            "ruta": "./img/a1.png"
-          },
-          cartaAdversario : {
-            "id": 2,
-            "lCarta": "A",
-            "nCarta": "2",
-            "nombre": "Flash",
-            "altura": 1.96,
-            "peso": 90,
-            "fuerza": 840,
-            "ganadas": 900,
-            "velocidad": 800000,
-            "ruta": "./assets/img/a2.png"
-          }
+    return (dispatch) =>  {
+        mazo = fetch("http://localhost:4000/cartas");
+        mazo
+        .then (res=>res.json())
+        .then (res=>{
+            dispatch({
+                type : "REPARTIR_MAZO",
+                mazo : res,
+                cartasJugador : cartasJugador,
+                cartasAdversario : cartasAdversario
+
+            })
+        })  
     }
 }
     
--- src/api/reducer.js
+++ src/api/reducer.js
@@ -2,41 +2,21 @@ const estadoInicial = {
     mazo: [],
     cartasJugador : [],
     cartasAdversario : [],
-    cartaJugador : {
-      "id": 0,
-      "lCarta": "",
-      "nCarta": "",
-      "nombre": "",
-      "altura": 0,
-      "peso": 0,
-      "fuerza": 0,
-      "ganadas": 0,
-      "velocidad": 0,
-      "ruta": ""
-    },
-    cartaAdversario : {
-      "id": 2,
-      "lCarta": "A",
-      "nCarta": "2",
-      "nombre": "Flash",
-      "altura": 1.96,
-      "peso": 90,
-      "fuerza": 840,
-      "ganadas": 900,
-      "velocidad": 800000,
-      "ruta": "./assets/img/a2.png"
-    }
+    turnoJugador : true ,
+    cartaJugador : 1,
+    cartaAdversario : 2
 };
 
 const reducer = (estadoPrevio = estadoInicial, action) => {
     switch(action.type){
 
         case "REPARTIR_MAZO" :
-            return {...estadoPrevio, 
+            return {...estadoPrevio,
+              mazo: action.mazo, 
               cartasJugador: action.cartasJugador, 
               cartasAdversario: action.cartasAdversario,
-              cartaJugador: action.cartaJugador,
-              cartaAdversario: action.cartaAdversario
+              cartaJugador: action.mazo[action.cartasJugador[0]],              
+              cartaAdversario: action.mazo[action.cartasAdversario[0]]
              };
 
         default :
