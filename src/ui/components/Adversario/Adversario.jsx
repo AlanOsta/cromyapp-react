d6da8b5f72d4017aa377f2f3369892b6662afb82
Alan Osta <alanosta@gmail.com>
2021-06-10 15:30:39 -0300

    Implementacion efecto cantidad de cartas

--- src/ui/components/Adversario/Adversario.jsx
+++ src/ui/components/Adversario/Adversario.jsx
@@ -1,16 +1,24 @@
 import './Adversario.css';
 import back from '../../img/back.png'
+import backpila from '../../img/backpila.png'
+import { connect } from "react-redux"
 
-const Adversario = () => {
+const Adversario = (props) => {
 
     let adversarioCard = 
             <div className="adversario">
-                <img src={back} alt="Adversario"></img>
+                {props.cartasAdversario.map( carta => <img src={backpila} key={"pila"+carta} alt="Adversario" className="img-adversario-pila"></img> )}
+                    
+                <img src={back} alt="Adversario" className="img-adversario"></img>
             </div>
 
     return (adversarioCard)
 }
 
-export default Adversario;
+const mapStateToProps = store => ({
+    cartasAdversario: store.cartasAdversario
+});
+
+export default connect(mapStateToProps, null)(Adversario);
 
         
